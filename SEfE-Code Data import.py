4ad2e39ad3d50e7f43f878ce59e7e493f52ac39b
Sergio Gobbi <34438859+weggli@users.noreply.github.com>
2017-12-11 14:36:32 +0100

    Add files via upload
    Hey Jungs! Habe mal die Packages eingetragen (wie besprochen heute morgen)!

---
+++ SEfE-Code Data import.py
@@ -0,0 +1,9 @@
+# Software Engineering Project:
+# **********************************************************************
+# Basics and Packages Used
+import os
+import panda as pd
+import matplotlib.pyplot as plt
+import numpy as np
+cwd = os.getcwd()
+os.chdir()
